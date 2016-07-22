7ebcbdb84861c62cbb18eef8ac5f8fbedd1747dc
jmnote <banking@nate.com>
2016-07-22 16:35:27 +0900

    Update vcap.inc.php

--- includes/vcap.inc.php
+++ includes/vcap.inc.php
@@ -4,15 +4,22 @@ $vcap_redis_servers = array();
 foreach($vcap_services as $service_provider => $service_list) {
 	foreach ($service_list as $service) {
 		if ( in_array('redis', $service['tags'], true) || strpos($service['name'], 'redis') !== false ) {
+			if( isset($service['credentials']['hostname']) ) $host = $service['credentials']['hostname'];
+			else if( isset($service['credentials']['host']) ) $host = $service['credentials']['host'];
+			else continue;
+			
+			if( !isset($service['credentials']['port']) ) continue;
+			if( !isset($service['credentials']['password']) ) continue;
+			
 			$vcap_redis_servers[] = array(
 				'name' => $service['name'],
-				'host' => $service['credentials']['hostname'],
+				'host' => $host,
 				'port' => $service['credentials']['port'],
 				'auth' => $service['credentials']['password'],
 				'filter' => '*',
 				'scheme' => 'tcp',
 				'path' => '',
-				);
+			);
 		}
 	}
 }
